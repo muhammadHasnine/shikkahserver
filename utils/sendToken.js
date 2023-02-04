eeed54b0c00b05bfc6c5dcd4e159652762063d47
muhammadHasnine <mdhasninesheak@gmail.com>
2023-02-04 14:24:05 +0600

    token setting

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -3,7 +3,7 @@ const sendToken = (res,user,message,statusCode)=>{
     const options ={
         expires:new Date(Date.now() + 15 * 24 * 60 * 60 * 1000),
         httpOnly:true,
-        // secure:true,
+        secure:true,
         sameSite:"none"
     }
     res.status(statusCode).cookie("token",token,options).json({
